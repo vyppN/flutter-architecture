43f7dcc71e5de56b5903cc83a4c039ace40fe5d0
vyppN <chwrtokuma@gmail.com>
2024-05-11 15:53:35 +0700

    add bottomtab navigation

---
+++ apps/core/lib/layouts/bottom_navigation.dart
@@ -0,0 +1,44 @@
+import 'package:core/router/router.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class BottomNavigation extends StatefulWidget {
+  final String location;
+  final Widget child;
+
+  const BottomNavigation({super.key, required this.child, required this.location});
+
+  @override
+  State<BottomNavigation> createState() => _BottomNavigationState();
+}
+
+class _BottomNavigationState extends State<BottomNavigation> {
+  int _currentIndex = 0;
+
+  void onTap(BuildContext context, int index) {
+    if(index == _currentIndex) return;
+
+    setState(() {
+      _currentIndex = index;
+    });
+
+    context.goNamed(routes[index]);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: widget.child,
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        onTap: (index) {
+          onTap(context, index);
+        },
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.shop), label: 'Shopping'),
+          BottomNavigationBarItem(icon: Icon(Icons.people), label: 'User'),
+        ],
+      ),
+    );
+  }
+}
--- apps/core/lib/router/router.dart
+++ apps/core/lib/router/router.dart
@@ -1,12 +1,23 @@
+import 'package:core/layouts/bottom_navigation.dart';
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:product/features/home/presentation/screens/home_screen.dart';
 import 'package:product/infrastructure/router/router.dart';
 
+final routes = [
+  'home'
+];
+
 final router = GoRouter(routes: [
-  GoRoute(
-    path: '/',
-    name: 'home',
-    builder: (context, state) => const ProductHomePage(),
-    routes: productRoutes
-  ),
+  ShellRoute(
+    pageBuilder: (context, state, child) {
+      return NoTransitionPage(child: BottomNavigation(
+        location: state.matchedLocation,
+        child: child,
+      ));
+    },
+      routes: [
+        GoRoute(path: '/', name: 'home', builder: (context, state) => const ProductHomePage(),)
+      ]
+  )
 ]);
