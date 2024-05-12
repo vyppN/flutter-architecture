03827692728766c206b7f5af6517c815b7e445eb
vyppN <chwrtokuma@gmail.com>
2024-05-12 13:49:39 +0700

    feat: apple theme

--- apps/core/lib/layouts/bottom_navigation.dart
+++ apps/core/lib/layouts/bottom_navigation.dart
@@ -1,18 +1,20 @@
 import 'package:core/router/router.dart';
+import 'package:core_ui/theme/theme_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
-class BottomNavigation extends StatefulWidget {
+class BottomNavigation extends ConsumerStatefulWidget {
   final String location;
   final Widget child;
 
   const BottomNavigation({super.key, required this.child, required this.location});
 
   @override
-  State<BottomNavigation> createState() => _BottomNavigationState();
+  ConsumerState<BottomNavigation> createState() => _BottomNavigationState();
 }
 
-class _BottomNavigationState extends State<BottomNavigation> {
+class _BottomNavigationState extends ConsumerState<BottomNavigation> {
   int _currentIndex = 0;
 
   void onTap(BuildContext context, int index) {
@@ -27,12 +29,14 @@ class _BottomNavigationState extends State<BottomNavigation> {
 
   @override
   Widget build(BuildContext context) {
+    final color = ref.watch(appThemeProvider).themeColor;
     return Scaffold(
       body: widget.child,
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
-        selectedItemColor: Colors.green,
-        unselectedItemColor: Colors.grey,
+        selectedItemColor: color.selectedItem,
+        unselectedItemColor: color.unSelectedItem,
+        backgroundColor: color.backgroundPrimary,
         currentIndex: _currentIndex,
         onTap: (index) {
           onTap(context, index);
--- apps/product/lib/features/home/presentation/screens/product_detail_screen.dart
+++ apps/product/lib/features/home/presentation/screens/product_detail_screen.dart
@@ -1,31 +1,44 @@
+import 'package:core_ui/theme/theme_provider.dart';
 import 'package:core_ui/widgets/elements/texts/price_text.dart';
 import 'package:core_ui/widgets/elements/texts/text_title.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 import 'dart:io' show Platform;
 
 import 'package:product/features/home/domain/entities/product.dart';
 
-class ProductDetailScreen extends StatefulWidget {
+class ProductDetailScreen extends ConsumerStatefulWidget {
   final ProductToDisplay product;
   const ProductDetailScreen({super.key, required this.product});
 
   @override
-  State<ProductDetailScreen> createState() => _ProductDetailScreenState();
+  ConsumerState<ProductDetailScreen> createState() => _ProductDetailScreenState();
 }
 
 String capitalize(String string) {
   return string[0].toUpperCase() + string.substring(1, string.length);
 }
 
-class _ProductDetailScreenState extends State<ProductDetailScreen> {
+class _ProductDetailScreenState extends ConsumerState<ProductDetailScreen> {
   @override
   Widget build(BuildContext context) {
     final product = widget.product;
+    final color = ref.watch(appThemeProvider).themeColor;
 
     return Scaffold(
       appBar: AppBar(
-        title: Text(capitalize(product.category)),
+        title: Text(capitalize(product.category), style: TextStyle(color: color.text),),
+        backgroundColor: color.backgroundPrimary,
+        leading: IconButton(
+          icon: Icon(
+            Icons.chevron_left,
+            color: color.text,
+            size: 32,
+          ), onPressed: context.pop,
+        ),
       ),
+      backgroundColor: color.backgroundPrimary,
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
@@ -46,13 +59,16 @@ class _ProductDetailScreenState extends State<ProductDetailScreen> {
                     mainAxisAlignment: MainAxisAlignment.end,
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      IconButton(onPressed: null, icon: Icon(Platform.isIOS ? Icons.ios_share : Icons.share)),
+                      IconButton(
+                          onPressed: null,
+                          icon: Icon(Platform.isIOS ? Icons.ios_share : Icons.share, color: color.text,)
+                      ),
                     ],
                   ),
                 ],
               ),
               const Divider(),
-              Text(product.description!),
+              Text(product.description!, style: TextStyle(color: color.text),),
               const SizedBox(height: 64,),
               FilledButton(
                 onPressed: (){print('ssss');},
